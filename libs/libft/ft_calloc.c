5782c78912254dfad80cd940e34d52990372af58
FREYA-max <fenitrarak2006@gmail.com>
2026-07-14 14:25:15 +0300

    building mlx

---
+++ libs/libft/ft_calloc.c
@@ -0,0 +1,16 @@
+
+#include "libft.h"
+
+void	*ft_calloc(size_t num, size_t size)
+{
+	void	*result;
+
+	if (num != 0 && (num * size) / num != size)
+		return (NULL);
+	result = malloc(num * size);
+	if (result == NULL)
+		return (NULL);
+	ft_memset(result, 0, num * size);
+	return (result);
+}
+
